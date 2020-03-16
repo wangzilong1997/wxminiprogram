51bce6c2dced5d1ce18ed5565303a0e25b27394a
wangzilong1997 <1321787581@qq.com>
2020-03-16 19:13:58 +0800

    4-10 数据绑定的扩展用法

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -12,7 +12,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      var post_content = {
+        date:"sep 18 2016",
+        title:"正是虾蟹肥壮时",
+        post_img:"/images/post/crab.png",
+        content:"正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时正是虾蟹肥壮时",
+        view_num:"112",
+        collect_num:"88",
+        author_img:"/images/avatar/1.png"
+      }
+    this.setData(post_content)
   },
 
   /**
