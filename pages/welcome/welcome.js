253f2d04f830d9014139a3151bb4ca8fd37b1698
wangzilong1997 <1321787581@qq.com>
2020-03-16 21:38:01 +0800

    4-12 小程序的事件机制--捕捉与回调

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,11 @@
 Page({
-  
+  onTap:function(){
+    //console.log("onTap")
+    // wx.navigateTo({
+    //   url: '../posts/posts'
+    // })
+    wx.redirectTo({
+      url: '../posts/posts'
+    })
+  }
 })
