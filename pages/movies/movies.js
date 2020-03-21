c1126ca995f34c97327be4326195d1fc479f768f
wangzilong1997 <1321787581@qq.com>
2020-03-21 12:08:48 +0800

    7-13 电影页面数据绑定（上）

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -10,15 +10,16 @@ Page({
 
   },
   onLoad:function(event){
-    var inTheatersUrl = app.globalData.dobanBase +  "/v2/movie/in_theaters";
-    var comingSoonUrl = app.globalData.dobanBase +  "/v2/movie/commig_soon";
-    var top250Url = app.globalData.dobanBase +  "/v2/movie/top250";
-    this.getMovieListData(top250Url)
+    var inTheatersUrl = app.globalData.dobanBase +  "/v2/movie/in_theaters" + "?start=0&count=3";
+    var comingSoonUrl = app.globalData.dobanBase + "/v2/movie/commig_soon" + "?start=0&count=3";
+    var top250Url = app.globalData.dobanBase + "/v2/movie/top250" + "?start=0&count=3";
+    //this.getMovieListData(top250Url)
     this.getMovieListData(inTheatersUrl)
-    this.getMovieListData(comingSoonUrl)
+    //this.getMovieListData(comingSoonUrl)
    
   },
   getMovieListData:function(target){
+    var that = this;
     wx.request({
       url: target,
       method: 'GET',
@@ -27,6 +28,7 @@ Page({
       },
       success: function (res) {
         console.log(res)
+        that.processDoubanData(res.data)
       },
       fail: function (res) {
         console.log(res)
@@ -35,6 +37,27 @@ Page({
         console.log(res)
       }
     })
+  },
+  processDoubanData:function(movieDouban){
+      var movies = [];
+      for(var idx in movieDouban.subjects){
+        var subject = movieDouban.subjects[idx];
+        var title = subject.title;
+        if(title.length >= 6){
+          title = title.substring(0,6) + "..."
+        }
+        var temp = {
+          title:title,
+          average:subject.rating.average,
+          coverageUrl:subject.images.large,
+          movieId:subject.id
+        }
+        movies.push(temp)
+        console.log(movies)
+      }
+      this.setData({
+        movies: movies
+      })
   }
 
 })
