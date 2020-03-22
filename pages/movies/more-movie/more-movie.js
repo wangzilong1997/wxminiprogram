a25ae516656348975c9eb963a5959cb4d7326879
wangzilong1997 <1321787581@qq.com>
2020-03-22 10:04:20 +0800

    8-3 更多电影页面加载数据

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -1,3 +1,5 @@
+var app = getApp();
+var util = require('../../../utils/util.js')
 // pages/movies/more-movie/more-movie.js
 Page({
 
@@ -5,6 +7,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    movies:{},
     navigateTitle:""
   },
 
@@ -14,7 +17,44 @@ Page({
   onLoad: function (options) {
     var category = options.category;
     this.data.navigateTitle = category;
-    console.log(category)
+    console.log(category);
+    var dataUrl = "";
+    switch (category){
+      case "正在热映":
+        dataUrl = app.globalData.dobanBase + "/v2/movie/in_theaters";
+        break;
+      case "即将上映":
+        dataUrl = app.globalData.dobanBase + "/v2/movie/coming_soon";
+        break;
+      case "豆瓣top250":
+        dataUrl = app.globalData.dobanBase + "/v2/movie/top250";
+        break;
+    }
+    util.http(dataUrl,this.processDoubanData)
+  },
+  processDoubanData: function(movieDouban){
+    var movies = [];
+    for (var idx in movieDouban.subjects) {
+      var subject = movieDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "..."
+      }
+      var temp = {
+        stars: util.convertToStarsArray(subject.rating.stars),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+      console.log(movies)
+    }
+  
+
+    this.setData({
+      movies:movies
+    })
   },
   onReady:function(event){
     wx.setNavigationBarTitle({
--- utils/util.js
+++ utils/util.js
@@ -1,16 +1,36 @@
-function convertToStarsArray(stars){
-  var num = stars.toString().substring(0,1);
+function convertToStarsArray(stars) {
+  var num = stars.toString().substring(0, 1);
   var array = [];
-  for(var i=0;i<5;i++){
-    if(i<num){
+  for (var i = 0; i < 5; i++) {
+    if (i < num) {
       array.push(1)
-    }else{
+    } else {
       array.push(0)
-    }  
+    }
   }
   return array;
 }
 
+function http(target,callBack) {
+  wx.request({
+    url: target,
+    method: 'GET',
+    header: {
+      "Content-Type": ""
+    },
+    success: function(res) {
+      console.log(res)
+      callBack(res.data)
+    },
+    fail: function(res) {
+      console.log(res)
+    },
+    complete: function(res) {
+      console.log(res)
+    }
+  })
+}
 module.exports = {
-  convertToStarsArray: convertToStarsArray
+  convertToStarsArray: convertToStarsArray,
+  http: http
 }
